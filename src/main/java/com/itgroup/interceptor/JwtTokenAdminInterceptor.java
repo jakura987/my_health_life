430285e04ff12518bdec74c7f2adf6ff84292141
Nick <jakuradyhdyh@gmail.com>
2023-09-04 20:51:22 +1000

    基本没啥问题了

--- src/main/java/com/itgroup/interceptor/JwtTokenAdminInterceptor.java
+++ src/main/java/com/itgroup/interceptor/JwtTokenAdminInterceptor.java
@@ -20,7 +20,8 @@ import javax.servlet.http.HttpServletResponse;
 public class JwtTokenAdminInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 直接放行 OPTIONS 请求
+        // 直接放行 OPTIONS 请求(前后端都部署在本地时, 前端加proxy,后端"addCrosMapping")
+        // 只有后端部署在服务器上时(记得改配置),前端不用加proxy,后端preHandler放行"OPTIONS"
         if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
             return true;
         }
