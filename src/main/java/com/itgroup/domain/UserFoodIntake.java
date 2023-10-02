6bfded9488e98dbd06842018006f70195d9127d5
Nick <jakuradyhdyh@gmail.com>
2023-10-02 14:57:24 +1100

    same problem

--- src/main/java/com/itgroup/domain/UserFoodIntake.java
+++ src/main/java/com/itgroup/domain/UserFoodIntake.java
@@ -1,6 +1,8 @@
 package com.itgroup.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
@@ -21,7 +23,9 @@ public class UserFoodIntake {
     private Long id;
     private Integer type;//breakfast, lunch, dinner
     private Long userId;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long foodId;
+    @JsonSerialize(using = ToStringSerializer.class)
     private Long categoryId;
     private String name;// name of food
     private Integer grams;
